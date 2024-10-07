b7b24680b2f02812f1174ca94d1c552c617d6a2f
Kamil Lewandowski <kamlew555@gmail.com>
2024-10-07 21:45:01 +0200

    FEAT: Create JSONManager class for managing saving and loading files to and from vehicle_database.json

---
+++ src/models/json_operations.py
@@ -0,0 +1,21 @@
+from src.models.vehicle_database import VehicleDatabase
+from src.models.vehicle import Vehicle
+import json
+
+
+class JSONManager:
+    def __init__(self):
+        self.vehicle_db = VehicleDatabase()
+
+    @staticmethod
+    def save_to_json(data: list, filename: str):
+        with open(filename, "w") as file:
+            serialized_data = [vehicle.to_dict() for vehicle in data]
+            json.dump(serialized_data, file, indent=4, ensure_ascii=False)
+            print("Data saved successfully to JSON")
+
+    @staticmethod
+    def load_from_json(filename: str) -> list:
+        with open(filename, "r") as file:
+            serialized_data = json.load(file)
+            return [Vehicle.from_dict(vehicle) for vehicle in serialized_data]
